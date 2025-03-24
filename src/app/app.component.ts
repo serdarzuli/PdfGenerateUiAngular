540ca09343357ae50297fdae0b16dc63b9d4dd4b
ZuliSerdar <serdar.zuli@gmail.com>
2025-03-24 21:21:46 +0100

    added new compoments for home pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+// import { HeaderComponent } from "./header/header.component";
+// import { FooterComponent } from "./footer/footer.component";
 import { NgFor, NgIf } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgFor, NgIf],
+  imports: [
+    RouterOutlet,
+    NgFor,
+    NgIf,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatCardModule,
+    DragDropModule,
+    CdkDrag,
+    CdkDropList,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,4 +46,21 @@ export class AppComponent {
     // 'logic 3 is',
     // 'logic 4 is'
   ];
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  processing = ['processing up groceries', 'processing home', 'Fall in love'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class TestService {
+export class TodoService {
 
-  callCenter = "444 0 9987879";
   constructor() { }
 }
