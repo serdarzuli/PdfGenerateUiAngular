8de091bba04109cdee5b30c2cf6edfd421afea6d
ZuliSerdar <serdar.zuli@gmail.com>
2025-04-08 11:23:13 +0200

    removed unnecessary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { HeaderComponent } from "./header/header.component";
-// import { FooterComponent } from "./footer/footer.component";
 import { NgFor, NgIf } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,7 +7,7 @@ import {MatCardModule} from '@angular/material/card';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
-
+import { FormsModule } from '@angular/forms';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -17,6 +15,8 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { TodoService } from './services/todo.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -33,23 +33,19 @@ import {
     CdkDrag,
     CdkDropList,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Serdar';
-  todos = [
-    // 'logic 1 is',
-    // 'logic 2 is',
-    // 'logic 3 is',
-    // 'logic 4 is'
-  ];
+  data: any = {};
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  processing = ['processing up groceries', 'processing home', 'Fall in love'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  constructor(
+    private todoService: TodoService,
+    private snackBar: MatSnackBar
+  ){}
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
@@ -63,4 +59,56 @@ export class AppComponent {
       );
     }
   }
+
+  addTodo(todo: { value: string; }) {
+    const obj = { todo: todo.value };
+    this.todoService.addTodo(obj)
+      .subscribe((res: any) => {
+        this.openSnackBar(res.message);
+        this.getAllTodos();
+        todo.value = '';
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Tamam', {
+      duration: 2000,
+    });
+  }
+
+  getAllTodos() {
+    this.todoService.getAllTodos()
+      .subscribe((res: any) => {
+        console.log(res);
+        Object.keys(res).forEach((key) => {
+          this.data[key] = res[key];
+        });
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  updateTodo() {
+    this.todoService.updateTodo(this.data)
+      .subscribe((res) => {
+        console.log(res);
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  removeTodo(id: string) {
+    if (confirm('Bu maddeyi silmek istediğinize emin misiniz?')) {
+      this.todoService.removeTodo(id)
+        .subscribe((res) => {
+          console.log(res);
+          this.getAllTodos();
+        }, (err) => {
+          console.log(err);
+        });
+    }
+  }
+  
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {inject, Inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    @Inject('API_URL') private apiUrl: string,
+  ) { }
+
+  addTodo(obj: { todo: any; }) {
+    return this.http.post(this.apiUrl + '/todo', obj);
+  }
+
+  getAllTodos() {
+    return this.http.get(this.apiUrl + '/todo');
+  }
+
+  updateTodo(obj: any) {
+    return this.http.put(this.apiUrl + '/todo', obj);
+  }
+
+  removeTodo(id: string) {
+    return this.http.delete(this.apiUrl + '/todo/' + id);
+  }
+
 }
